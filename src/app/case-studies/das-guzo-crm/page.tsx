fda37cc12bc53f43d3628d9a57569d381f65bccf
alexdev93 <alemayehu.dev@gmail.com>
2025-07-05 23:15:26 +0300

    Create a single case study detail view

---
+++ src/app/case-studies/das-guzo-crm/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CaseStudies from "@/components/case-studies/";
+const page = () => {
+  return (
+    <div>
+      <CaseStudies />
+    </div>
+  )
+}
+
+export default page;
