ec619f10d0226cda11ec010bca6ef1b19e4bc630
alexdev93 <alemayehu.dev@gmail.com>
2025-07-11 02:10:48 +0300

    Refactor next.config.ts to clean up unused protocol and hostname variables

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,21 @@
 import type { NextConfig } from "next";
-const PROTOCOL = process.env.NEXT_PUBLIC_PROTOCOL || "https";
-const HOSTNAME =
-  process.env.NEXT_PUBLIC_HOSTNAME ||
-  "graceful-bubble-4a6d1d121b.strapiapp.com";
+// const PROTOCOL = process.env.NEXT_PUBLIC_PROTOCOL || "https";
+// const HOSTNAME =
+//   process.env.NEXT_PUBLIC_HOSTNAME ||
+//   "graceful-bubble-4a6d1d121b.media.strapiapp.com";
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol:
-          PROTOCOL === "http" || PROTOCOL === "https" ? PROTOCOL : "https",
-        hostname: HOSTNAME,
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+      },
+      {
+        protocol: "https",
+        hostname: "graceful-bubble-4a6d1d121b.media.strapiapp.com",
       },
     ],
   },
