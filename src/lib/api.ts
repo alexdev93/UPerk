c948d201b906148728d7615ac0548e15d6510252
alexdev93 <alemayehu.dev@gmail.com>
2025-07-11 10:30:46 +0300

    add fallback for th API_URL

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,7 @@
 // lib/api.ts
-const API_URL = process.env.NEXT_PUBLIC_STRAPI_API_URL;
+const API_URL =
+  process.env.NEXT_PUBLIC_STRAPI_API_URL ||
+  "https://graceful-bubble-4a6d1d121b.strapiapp.com/api";
 
 export async function fetchFromStrapi(endpoint: string, options = {}) {
   const response = await fetch(`${API_URL}/${endpoint}`, {
